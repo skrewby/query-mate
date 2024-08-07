abe01e8aaf63cd3a430ed40b575e1b68717c0a47
skrewby <33297928+skrewby@users.noreply.github.com>
2024-08-08 02:16:53 +1000

    Add option to copy to clipboard

--- src/pages/builder/UpdateForm.tsx
+++ src/pages/builder/UpdateForm.tsx
@@ -30,11 +30,17 @@ import { PlusCircle, XCircle } from "lucide-react"
 import { cn } from "@/lib/utils"
 import { useState } from "react"
 
+enum SubmitType {
+  Download,
+  Clipboard,
+  Preview,
+}
+
 interface FormProps {
   csv: Papa.ParseResult<any> | null,
   headers: string[]
 }
- 
+
 const formSchema = z.object({
   tablename: z.string().min(1).max(255),
   setfields: z.array(z.object({ column: z.string().min(1).max(32), value: z.string().min(1) })).min(1),
@@ -48,8 +54,8 @@ type whereFieldsType = z.infer<typeof formSchema>["wherefields"];
 function BuildQuery(data: any, table: string, setFields: setFieldsType, whereFields: whereFieldsType, whereCustom: string): string {
   var query = `UPDATE ${table} SET`;
   setFields.map((set, index) => {
-    if(index != 0) {
-      query += ','; 
+    if (index != 0) {
+      query += ',';
     }
     const setValue = data[set.value];
     query += ` ${set.column} = '${setValue}'`;
@@ -57,15 +63,15 @@ function BuildQuery(data: any, table: string, setFields: setFieldsType, whereFie
   query += ' WHERE';
 
   whereFields.map((val, index) => {
-    if(index != 0) {
-      query += ' AND'; 
+    if (index != 0) {
+      query += ' AND';
     }
     const whereValue = data[val.value];
     query += ` ${val.condition} = '${whereValue}'`;
   });
 
-  if(whereCustom.length > 0) {
-    if(whereFields.length > 0) {
+  if (whereCustom.length > 0) {
+    if (whereFields.length > 0) {
       query += ' AND';
     }
     query += ` ${whereCustom}`;
@@ -76,15 +82,25 @@ function BuildQuery(data: any, table: string, setFields: setFieldsType, whereFie
   return query;
 }
 
-function UpdateForm({csv, headers}: FormProps) {
-  const [showPreview, setShowPreview] = useState(false);
+function ParseDataFromForm(csv: Papa.ParseResult<any> | null, values: z.infer<typeof formSchema>): string {
+  var query = "";
+  csv?.data.map((row) => {
+    const rowQuery = BuildQuery(row, values.tablename, values.setfields, values.wherefields, values.wherecustom);
+    query += `${rowQuery}\n`;
+  });
+
+  return query;
+}
+
+function UpdateForm({ csv, headers }: FormProps) {
   const [preview, setPreview] = useState("");
+  const [submitType, setSubmitType] = useState<SubmitType>(SubmitType.Download);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       tablename: "",
-      setfields: [{column: "", value: ""}],
-      wherefields: [{condition: "", value: ""}],
+      setfields: [{ column: "", value: "" }],
+      wherefields: [{ condition: "", value: "" }],
       wherecustom: "",
     },
   })
@@ -99,23 +115,40 @@ function UpdateForm({csv, headers}: FormProps) {
     control: form.control
   });
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
-    if(showPreview) {
-      const query = BuildQuery(csv?.data[0], values.tablename, values.setfields, values.wherefields, values.wherecustom);
-      setPreview(query);
-    } else {
-      var query = "";
-      csv?.data.map((row) => {
-        const rowQuery = BuildQuery(row, values.tablename, values.setfields, values.wherefields, values.wherecustom);
-        query += `${rowQuery}\n`;
-      });
+  const downloadQuery = (query: string) => {
+    const element = document.createElement("a");
+    const file = new Blob([query], { type: 'text/csv' });
+    element.href = URL.createObjectURL(file);
+    element.download = "queries.sql";
+    document.body.appendChild(element); // Required for this to work in FireFox
+    element.click();
+  }
 
-      const element = document.createElement("a");
-      const file = new Blob([query], {type: 'text/csv'});
-      element.href = URL.createObjectURL(file);
-      element.download = "queries.sql";
-      document.body.appendChild(element); // Required for this to work in FireFox
-      element.click();
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    switch (submitType) {
+      case SubmitType.Download: {
+        const query = ParseDataFromForm(csv, values);
+        downloadQuery(query);
+        break;
+      }
+      case SubmitType.Clipboard: {
+        const query = ParseDataFromForm(csv, values);
+        await copyQueryToClipboard(query);
+        break;
+      }
+      case SubmitType.Preview: {
+        const q = BuildQuery(csv?.data[0], values.tablename, values.setfields, values.wherefields, values.wherecustom);
+        setPreview(q);
+        break;
+      }
+    }
+  }
+
+  const copyQueryToClipboard = async (q: string) => {
+    try {
+      await navigator.clipboard.writeText(q);
+    } catch (error: any) {
+      console.error(error.message);
     }
   }
 
@@ -138,7 +171,7 @@ function UpdateForm({csv, headers}: FormProps) {
           />
           <Label>SET</Label>
           <div className="border border-dashed p-4 flex flex-col gap-y-4">
-            {fields.map((field, index) => ( 
+            {fields.map((field, index) => (
               <div key={field.id} className="flex gap-x-8">
                 <div className="flex-1">
                   <FormField
@@ -147,17 +180,17 @@ function UpdateForm({csv, headers}: FormProps) {
                     render={({ field }) => (
                       <FormItem>
                         <FormLabel className={cn(index !== 0 && "sr-only")}>
-                          Column 
+                          Column
                         </FormLabel>
                         <FormDescription className={cn(index !== 0 && "sr-only")}>
-                          Database column name 
+                          Database column name
                         </FormDescription>
                         <FormControl>
                           <Input {...field} />
                         </FormControl>
                         <FormMessage />
                       </FormItem>
-                  )}/>
+                    )} />
                 </div>
                 <div className="flex-1">
                   <FormField
@@ -166,7 +199,7 @@ function UpdateForm({csv, headers}: FormProps) {
                     render={({ field }) => (
                       <FormItem>
                         <FormLabel className={cn(index !== 0 && "sr-only")}>
-                          Value 
+                          Value
                         </FormLabel>
                         <FormDescription className={cn(index !== 0 && "sr-only")}>
                           The value from the CSV to use
@@ -188,16 +221,16 @@ function UpdateForm({csv, headers}: FormProps) {
                         </div>
                         <FormMessage />
                       </FormItem>
-                    )}/>
-                  </div>
+                    )} />
+                </div>
               </div>
             ))}
-            <Button type="button" size="sm" className="m-auto sm bg-background text-foreground hover:bg-background p-2" onClick={() => append({column: "", value: ""})}><PlusCircle /></Button>
+            <Button type="button" size="sm" className="m-auto sm bg-background text-foreground hover:bg-background p-2" onClick={() => append({ column: "", value: "" })}><PlusCircle /></Button>
           </div>
 
           <Label>WHERE</Label>
           <div className="border border-dashed p-4 flex flex-col gap-y-4">
-            {whereFields.map((field, index) => ( 
+            {whereFields.map((field, index) => (
               <div key={field.id} className="flex gap-x-8">
                 <div className="flex-1">
                   <FormField
@@ -206,17 +239,17 @@ function UpdateForm({csv, headers}: FormProps) {
                     render={({ field }) => (
                       <FormItem>
                         <FormLabel className={cn(index !== 0 && "sr-only")}>
-                          Column 
+                          Column
                         </FormLabel>
                         <FormDescription className={cn(index !== 0 && "sr-only")}>
-                          Database column name 
+                          Database column name
                         </FormDescription>
                         <FormControl>
                           <Input {...field} />
                         </FormControl>
                         <FormMessage />
                       </FormItem>
-                  )}/>
+                    )} />
                 </div>
                 <div className="flex-1">
                   <FormField
@@ -225,7 +258,7 @@ function UpdateForm({csv, headers}: FormProps) {
                     render={({ field }) => (
                       <FormItem>
                         <FormLabel className={cn(index !== 0 && "sr-only")}>
-                          Value 
+                          Value
                         </FormLabel>
                         <FormDescription className={cn(index !== 0 && "sr-only")}>
                           The value from the CSV to use
@@ -247,11 +280,11 @@ function UpdateForm({csv, headers}: FormProps) {
                         </div>
                         <FormMessage />
                       </FormItem>
-                    )}/>
-                  </div>
+                    )} />
+                </div>
               </div>
             ))}
-            <Button type="button" size="sm" className="m-auto sm bg-background text-foreground hover:bg-background p-2" onClick={() => whereAppend({condition: "", value: ""})}><PlusCircle /></Button>
+            <Button type="button" size="sm" className="m-auto sm bg-background text-foreground hover:bg-background p-2" onClick={() => whereAppend({ condition: "", value: "" })}><PlusCircle /></Button>
           </div>
 
           <FormField
@@ -270,14 +303,15 @@ function UpdateForm({csv, headers}: FormProps) {
 
           <div className="space-x-2 flex justify-end">
             <Popover>
-              <PopoverTrigger><Button type="submit" onClick={() => setShowPreview(true)}>Preview</Button></PopoverTrigger>
+              <PopoverTrigger><Button type="submit" onClick={() => setSubmitType(SubmitType.Preview)}>Preview</Button></PopoverTrigger>
               <PopoverContent className="w-[calc(100vh-10px)] bg-secondary mx-auto">{preview}</PopoverContent>
             </Popover>
-            <Button type="submit" onClick={() => setShowPreview(false)}>Create Queries</Button>
+            <Button type="submit" onClick={() => setSubmitType(SubmitType.Clipboard)}>Copy to Clipboard</Button>
+            <Button type="submit" onClick={() => setSubmitType(SubmitType.Download)}>Download</Button>
           </div>
         </div>
       </form>
-    </Form>
+    </Form >
   );
 }
 
